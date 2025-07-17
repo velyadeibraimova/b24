c9b7161ba789031cef7ec786795cf1242dda790f
velyadeibraimova <velyade.ibraimova@yandex.ru>
2025-07-17 19:26:16 +0300

    EventHandlers

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Bitrix\Main\EventManager;
+
 /*
 * Автозагрузка composer
 * */
@@ -33,7 +35,7 @@ if (file_exists(__DIR__ . '/events.php')) {
 if (file_exists(__DIR__ . '/classes/Dadata.php')) {
     include_once __DIR__ . '/classes/Dadata.php';
 
-} 
+}
 function pr($var, $type = false)
 {
     echo '<pre style="font-size:10px; border:1px solid #000; background:#FFF; text-align:left; color:#000;">';
@@ -51,3 +53,15 @@ function pr($var, $type = false)
 ]);
 
 //\Bitrix\Main\UI\Extension::load(['otus.workday_confirm']);
+
+EventManager::getInstance()->addEventHandler(
+    'iblock',
+    'OnAfterIBlockElementUpdate',
+    ['Otus\EventHandlers\Iblock', 'updateDealAfterRequestChange']
+);
+
+EventManager::getInstance()->addEventHandler(
+    'crm',
+    'OnAfterCrmDealUpdate',
+    ['Otus\EventHandlers\Crm', 'updateRequestsAfterDealChange']
+);
--- local/php_interface/src/Otus/EventHandlers/Iblock.php
+++ local/php_interface/src/Otus/EventHandlers/Iblock.php
@@ -16,27 +16,21 @@ class Iblock
             'filter' => ['CODE' => 'requests'],
             'select' => ['ID']
         ])->fetch();
-
         //если список не тот, прекращаем обработку
         if (!$iblock || $arItem['IBLOCK_ID'] != $iblock['ID']) {
             return false;
         }
-
         //получить свойства
         $arPropertyList = PropertyTable::getList([
             'filter' => ['=IBLOCK_ID' => $iblock['ID']],
             'select' => ['ID', 'CODE']
         ])->fetchAll();
-
         $arProperty = [];
-
         //пересобрать свойства, чтобы было удобнее работать
         foreach ($arPropertyList as $property) {
             $id = $property['ID'];
             $code = $property['CODE'];
-
             $values = $arItem['PROPERTY_VALUES'][$id] ?? null;
-
             if (is_array($values)) {
                 $first = reset($values);
                 $arProperty[$code] = $first['VALUE'] ?? null;
@@ -44,31 +38,25 @@ class Iblock
                 $arProperty[$code] = $values;
             }
         }
-
         //отрезать валюту и форматировать сумму для сравнения
         [$amount, $currency] = array_pad(explode('|', $arProperty['AMOUNT']), 2, null);
         $arProperty['AMOUNT'] = number_format($amount, 2, '.', '');
-
         $dealId = $arProperty['DEAL'] ?? null;
         if (!$dealId) {
             return false;
         }
-
         //получить данные сделки
         $deal = DealTable::getList([
             'filter' => ['=ID' => $dealId],
             'select' => ['ID', 'OPPORTUNITY', 'ASSIGNED_BY_ID']
         ])->fetch();
-
         if (!$deal) {
             return false;
         }
-
         //если ничего не изменилось, прервать работу
         if (($arProperty['AMOUNT'] === $deal['OPPORTUNITY']) && ($arProperty['RESPONSIBLE'] === $deal['ASSIGNED_BY_ID'])) {
             return false;
         }
-
         //обновить данные
         try {
             $result = DealTable::update($dealId, [
@@ -78,11 +66,9 @@ class Iblock
         } catch (\Exception $e) {
             return false;
         }
-
         if (!$result->isSuccess(true)) {
             return false;
         }
-
         return true;
     }
 }
